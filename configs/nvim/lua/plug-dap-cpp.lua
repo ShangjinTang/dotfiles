e7753d1fe1d31ff9cbd03d0c8e9993e4413ad9fe
Shangjin Tang <shangjin.tang@gmail.com>
2023-05-06 04:59:46 +0800

    Major: add plug-dap-cpp using vscode-cpptools

---
+++ configs/nvim/lua/plug-dap-cpp.lua
@@ -0,0 +1,21 @@
+local dap = require('dap')
+
+-- Requires: ':MasonInstall cpptools'
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = vim.fn.expand('~/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7'),
+}
+
+dap.configurations.cpp = {
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+    },
+}
