24bbe02ac7e532cbfc9525c328f6f7b553b4602f
Shangjin Tang <shangjin.tang@gmail.com>
2023-05-09 00:31:17 +0800

    Major: lazy.nvim: add wilder

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -2,13 +2,12 @@
  THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
  `lvim` is the global options object
 ]]
-
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+    enabled = true,
+    pattern = "*.lua",
+    timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -101,15 +100,99 @@ lvim.plugins = {
     --   "folke/trouble.nvim",
     --   cmd = "TroubleToggle",
     -- },
+
     {
         -- Reference: https://github.com/kylechui/nvim-surround
         "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        version = "*",
         event = "VeryLazy",
         config = function()
             require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
             })
+        end
+    },
+
+    {
+        -- Reference: https://github.com/gelguy/wilder.nvim
+        "gelguy/wilder.nvim",
+        lazy = true,
+        event = "CmdlineEnter",
+        dependencies = {
+            "romgrk/fzy-lua-native",
+        },
+        config = function()
+            local wilder = require('wilder')
+            wilder.setup({ modes = { ':', '/', '?' } })
+
+            wilder.set_option('pipeline', {
+                wilder.branch(
+                    wilder.python_file_finder_pipeline({
+                        file_command = { 'rg', '--files' },
+                        dir_command = { 'fd', '-td' },
+                        filters = { 'fuzzy_filter', 'difflib_sorter' },
+                    }),
+                    wilder.substitute_pipeline({
+                        pipeline = wilder.python_search_pipeline({
+                            skip_cmdtype_check = 1,
+                            pattern = wilder.python_fuzzy_pattern({
+                                start_at_boundary = 0,
+                            }),
+                        }),
+                    }),
+                    wilder.cmdline_pipeline({
+                        fuzzy = 2,
+                        fuzzy_filter = wilder.lua_fzy_filter(),
+                    }),
+                    {
+                        wilder.check(function(ctx, x) return x == '' end),
+                        wilder.history(),
+                    },
+                    wilder.python_search_pipeline({
+                        pattern = wilder.python_fuzzy_pattern({
+                            start_at_boundary = 0,
+                        }),
+                    })
+                ),
+            })
+
+            local highlighters = {
+                wilder.pcre2_highlighter(),
+                wilder.lua_fzy_highlighter(),
+            }
+
+            local popupmenu_renderer = wilder.popupmenu_renderer(
+                wilder.popupmenu_border_theme({
+                    border = 'rounded',
+                    winblend = 20,
+                    empty_message = wilder.popupmenu_empty_message_with_spinner(),
+                    highlighter = highlighters,
+                    left = {
+                        ' ',
+                        wilder.popupmenu_devicons(),
+                        wilder.popupmenu_buffer_flags({
+                            flags = ' a + ',
+                            icons = { ['+'] = '', a = '', h = '' },
+                        }),
+                    },
+                    right = {
+                        ' ',
+                        wilder.popupmenu_scrollbar(),
+                    },
+                })
+            )
+
+            local wildmenu_renderer = wilder.wildmenu_renderer({
+                highlighter = highlighters,
+                separator = ' · ',
+                left = { ' ', wilder.wildmenu_spinner(), ' ' },
+                right = { ' ', wilder.wildmenu_index() },
+            })
+
+            wilder.set_option('renderer', wilder.renderer_mux({
+                [':'] = popupmenu_renderer,
+                ['/'] = wildmenu_renderer,
+                substitute = wildmenu_renderer,
+            }))
         end
     },
 }
