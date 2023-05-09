b751dfdc94815a2ed6bd96e455f4fa27d5e185af
Shangjin Tang <shangjin.tang@gmail.com>
2023-05-09 12:02:19 +0800

    Major: nvim: move configurations of some plugins from init.vim to config.lua

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -250,6 +250,19 @@ lvim.plugins = {
     {
         "folke/trouble.nvim",
         cmd = "TroubleToggle",
+        config = function()
+            local actions = require("telescope.actions")
+            local trouble = require("trouble.providers.telescope")
+            local telescope = require("telescope")
+            telescope.setup {
+                defaults = {
+                    mappings = {
+                        i = { ["<c-t>"] = trouble.open_with_trouble },
+                        n = { ["<c-t>"] = trouble.open_with_trouble },
+                    },
+                },
+            }
+        end
     },
 
     -- Reference: https://github.com/dstein64/vim-startuptime
