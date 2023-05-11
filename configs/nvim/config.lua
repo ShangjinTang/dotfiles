ad126f7c4e6111a38e2d8a04908f6dde7957b9a5
Shangjin Tang <shangjin.tang@gmail.com>
2023-05-11 22:13:36 +0800

    Major: cmakebuild: use build/ directory instead of .build

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -243,7 +243,7 @@ lvim.plugins = {
     {
         "iamcco/markdown-preview.nvim",
         ft = "markdown",
-        cmd = {"MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle"},
+        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
         event = "VeryLazy",
         config = function()
             vim.fn["mkdp#util#install"]()
@@ -395,11 +395,11 @@ lvim.plugins = {
                     },
                 },
                 presets = {
-                    bottom_search = true, -- use a classic bottom cmdline for search
-                    command_palette = true, -- position the cmdline and popupmenu together
+                    bottom_search = true,         -- use a classic bottom cmdline for search
+                    command_palette = true,       -- position the cmdline and popupmenu together
                     long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = true,  -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = true, -- add a border to hover docs and signature help
+                    inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = true,        -- add a border to hover docs and signature help
                 },
             })
         end
