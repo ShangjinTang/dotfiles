087f05a5f384db814b5b75f408305336fb6de5a8
Shangjin Tang <shangjin.tang@gmail.com>
2023-06-09 15:07:35 +0800

    Minor: ChatGPT.nvim: increase max_tokens from 300 to 2048

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -412,6 +412,15 @@ lvim.plugins = {
                         submit = "<C-Enter>",
                     }
                 },
+                openai_params = {
+                    model = "gpt-3.5-turbo",
+                    frequency_penalty = 0,
+                    presence_penalty = 0,
+                    max_tokens = 2048,
+                    temperature = 0,
+                    top_p = 1,
+                    n = 1,
+                },
             })
         end
     },
