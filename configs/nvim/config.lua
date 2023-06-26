fb554e712ac07efc1bf55b0742dc2a12c730d8df
Shangjin Tang <shangjin.tang@gmail.com>
2023-06-26 09:16:54 +0800

    Major: nvim: add neorg

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -597,6 +597,30 @@ lvim.plugins = {
         end
     },
 
+    -- Reference: https://github.com/nvim-neorg/neorg
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            vim.cmd([[ set conceallevel=2 ]])
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {},  -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = {      -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                            default_workspace = "notes",
+                        },
+                    },
+                },
+            }
+        end
+    },
+
     -- Reference: https://github.com/SmiteshP/nvim-navic
     {
         "SmiteshP/nvim-navic",
