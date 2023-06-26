ShangjinTang/dotfiles

commit 0e6c76aee213d62cd3034fe7d6c6f75969ad4c79

Minor: norg: change default workspace from '~/notes' to '~/norg/note'; add completion 'nvim-cmp'

configs/nvim/config.lua
@@ -602,21 +602,34 @@ lvim.plugins = {
     {
         "nvim-neorg/neorg",
         build = ":Neorg sync-parsers",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp"
+        },
         config = function()
             vim.cmd([[ set conceallevel=2 ]])
             require("neorg").setup {
                 load = {
-                    ["core.defaults"] = {},  -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = {      -- Manages Neorg workspaces
+                    ["core.defaults"] = {},
+                    ["core.completion"] = {
+                        config = {
+                            engine = "nvim-cmp",
+                        }
+                    },
+                    ["core.concealer"] = {},
+                    ["core.dirman"] = {
                         config = {
                             workspaces = {
-                                notes = "~/notes",
+                                note = "~/norg/note",
                             },
-                            default_workspace = "notes",
+                            default_workspace = "note",
                         },
                     },
+                },
+            }
+            require("cmp").setup {
+                sources = {
+                    { name = "neorg" },
                 },
             }
         end

commit 79e702f0ea744a87f0dc78e71d24c583e6fbde2a

Major: nvim: norg: fix issue caused by 0e6c76a and 643936e
1. avoid create ~/norg everytime on nvim enter; use _DIRCREATE instead
2. fix nvim-cmp cannot auto complete issue by removing nvim-cmp integration with norg

configs/nvim/config.lua
@@ -604,12 +604,9 @@ lvim.plugins = {
         build = ":Neorg sync-parsers",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp"
         },
         config = function()
-            local norg_root_path = vim.fn.expand('~/norg') .. '/'
             vim.cmd([[ set conceallevel=2 ]])
-            vim.cmd("!mkdir -p " .. norg_root_path)
             require("neorg").setup {
                 load = {
                     ["core.defaults"] = {},
@@ -622,16 +619,11 @@ lvim.plugins = {
                     ["core.dirman"] = {
                         config = {
                             workspaces = {
-                                note = norg_root_path .. "note",
+                                note = "~/norg/note",
                             },
                             default_workspace = "note",
                         },
                     },
-                },
-            }
-            require("cmp").setup {
-                sources = {
-                    { name = "neorg" },
                 },
             }
         end

configs/zsh/zshrc.pre
@@ -155,12 +155,15 @@ _PATHAPPEND ~/.cargo/bin
 # For luarocks binaries, such as lua format tool 'formatterfiveone'
 _PATHAPPEND ~/.luarocks/bin
 
-# Creating npm local directory for nvim lsp
+# Create npm local directory for nvim lsp
 # Reference: https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally#manually-change-npms-default-directory
 _DIRCREATE ~/.npm-global/bin && _PATHAPPEND ~/.npm-global/bin && export NPM_CONFIG_PREFIX=~/.npm-global
 
-# Creating nvim persistent undo directory
+# Create nvim persistent undo directory
 _DIRCREATE ~/.cache/nvim/undo
+
+# Create norg directory
+_DIRCREATE ~/norg
 
 #=====================================================================
 # fuctions for command line use