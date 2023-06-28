8cba2d3a3cde50dd4c9c217254aa452768434203
Shangjin Tang <shangjin.tang@gmail.com>
2023-06-29 01:51:32 +0800

    Minor: nvim: autocmd: move logic 'insert toggle line numbers' to config.lua

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -1001,6 +1001,10 @@ lspconfig.marksman.setup({
 -- specify the python3 we use as nvim python
 vim.g.python3_host_prog = os.getenv("PYTHON3_HOST_PROG")
 
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 
 -- let treesitter use bash highlight for zsh files as well
@@ -1050,3 +1054,31 @@ vim.api.nvim_create_autocmd(
         end,
     }
 )
+
+--------------------------------------------------------------------------------
+
+vim.api.nvim_create_autocmd(
+    {
+        "InsertEnter",
+    },
+    {
+        callback = function()
+            vim.cmd([[ set nonumber ]])
+            vim.cmd([[ set norelativenumber ]])
+        end,
+    }
+)
+
+vim.api.nvim_create_autocmd(
+    {
+        "InsertLeave",
+    },
+    {
+        callback = function()
+            vim.cmd([[ set number ]])
+            vim.cmd([[ set relativenumber ]])
+        end,
+    }
+)
+
+--------------------------------------------------------------------------------
