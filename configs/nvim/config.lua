a7d9a40e57fa31aa09c4c31c3252219feddc52bf
Shangjin Tang <shangjin.tang@gmail.com>
2023-07-05 23:24:49 +0800

    Major: nvim(lvim): remove vim-startuptime for lvim <leader>Pp already have profiler for startup time

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -893,15 +893,6 @@ lvim.plugins = {
         end,
     },
 
-    -- Reference: https://github.com/dstein64/vim-startuptime
-    {
-        "dstein64/vim-startuptime",
-        cmd = "StartupTime",
-        init = function()
-            vim.g.startuptime_tries = 10
-        end,
-    },
-
     -- Reference: https://github.com/nvim-neotest/neotest
     {
         "nvim-neotest/neotest",
