4e88f3cfcde20b0b781f7d6d0acceae3f3a46ce7
Shangjin Tang <shangjin.tang@gmail.com>
2023-07-14 09:22:14 +0800

    Major: nvim: add mkdnflow.nvim for markdown

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -529,6 +529,67 @@ lvim.plugins = {
         end,
     },
 
+    -- Reference: https://github.com/jakewvincent/mkdnflow.nvim
+    {
+        "jakewvincent/mkdnflow.nvim",
+        ft = "markdown",
+        event = "VeryLazy",
+        config = function()
+            require("mkdnflow").setup({
+                modules = {
+                    bib = true,
+                    buffers = true,
+                    conceal = true,
+                    cursor = true,
+                    folds = true,
+                    links = true,
+                    maps = true,
+                    lists = true,
+                    paths = true,
+                    tables = true,
+                    yaml = false,
+                },
+                mappings = {
+                    MkdnEnter = { { "n", "v" }, "<CR>" },
+                    MkdnTab = false,
+                    MkdnSTab = false,
+                    MkdnNextLink = { "n", "<Tab>" },
+                    MkdnPrevLink = { "n", "<S-Tab>" },
+                    MkdnNextHeading = { "n", "]]" },
+                    MkdnPrevHeading = { "n", "[[" },
+                    MkdnGoBack = { "n", "<BS>" },
+                    MkdnGoForward = { "n", "<Del>" },
+                    MkdnCreateLink = false, -- see MkdnEnter
+                    MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>p" }, -- see MkdnEnter
+                    MkdnFollowLink = false, -- see MkdnEnter
+                    MkdnDestroyLink = { "n", "<M-CR>" },
+                    MkdnTagSpan = { "v", "<M-CR>" },
+                    MkdnMoveSource = { "n", "<F2>" },
+                    MkdnYankAnchorLink = { "n", "yaa" },
+                    MkdnYankFileAnchorLink = { "n", "yfa" },
+                    MkdnIncreaseHeading = { "n", "+" },
+                    MkdnDecreaseHeading = { "n", "-" },
+                    MkdnToggleToDo = { { "n", "v" }, "<C-Space>" },
+                    MkdnNewListItem = false,
+                    MkdnNewListItemBelowInsert = { "n", "o" },
+                    MkdnNewListItemAboveInsert = { "n", "O" },
+                    MkdnExtendList = false,
+                    MkdnUpdateNumbering = { "n", "<leader>nn" },
+                    MkdnTableNextCell = { "i", "<Tab>" },
+                    MkdnTablePrevCell = { "i", "<S-Tab>" },
+                    MkdnTableNextRow = false,
+                    MkdnTablePrevRow = { "i", "<M-CR>" },
+                    MkdnTableNewRowBelow = { "n", "<leader>ir" },
+                    MkdnTableNewRowAbove = { "n", "<leader>iR" },
+                    MkdnTableNewColAfter = { "n", "<leader>ic" },
+                    MkdnTableNewColBefore = { "n", "<leader>iC" },
+                    MkdnFoldSection = { "n", "<leader>f" },
+                    MkdnUnfoldSection = { "n", "<leader>F" },
+                },
+            })
+        end,
+    },
+
     -- Reference: https://github.com/dhruvasagar/vim-table-mode
     {
         "dhruvasagar/vim-table-mode",
