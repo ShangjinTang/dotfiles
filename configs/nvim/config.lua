414d7c9cfcfc0328d7e7020c14ff096533d5c017
Shangjin Tang <shangjin.tang@gmail.com>
2023-07-20 03:11:29 +0800

    Major: nvim: add nvim-treesitter-textobjects

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -932,6 +932,68 @@ lvim.plugins = {
         end,
     },
 
+    -- Reference: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ["@parameter.outer"] = "v", -- charwise
+                            ["@function.outer"] = "V", -- linewise
+                            ["@class.outer"] = "<c-v>", -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            ["]f"] = "@function.outer",
+                            ["]b"] = "@block.outer",
+                            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                            ["]o"] = "@loop.*",
+                            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+                            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                        },
+                        goto_previous_start = {
+                            ["[f"] = "@function.outer",
+                            ["[b"] = "@block.outer",
+                            ["[c"] = "@class.outer",
+                        },
+                        goto_next = {
+                            ["]d"] = "@conditional.outer",
+                        },
+                        goto_previous = {
+                            ["[d"] = "@conditional.outer",
+                        },
+                    },
+                },
+            })
+        end,
+    },
+
     -- Reference: https://github.com/gelguy/wilder.nvim
     {
         "gelguy/wilder.nvim",
