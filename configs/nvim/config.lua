11a867713e3e4da9e692ce7b6bcb876eae331891
shangjin.tang <shangjin.tang@gmail.com>
2023-07-20 23:34:25 +0800

    Major: nvim: add sniprun

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -829,6 +829,31 @@ lvim.plugins = {
         end,
     },
 
+    -- Reference: https://github.com/michaelb/sniprun
+    {
+        "michaelb/sniprun",
+        event = "VeryLazy",
+        build = "sh ./install.sh 1",
+        config = function()
+            require("sniprun").setup({
+                display = {
+                    -- "Classic", --# display results in the command-line  area
+                    -- "VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
+
+                    -- "VirtualText", --# display results as virtual text
+                    "TempFloatingWindow", --# display results in a floating window
+                    -- "LongTempFloatingWindow", --# same as above, but only long results. To use with VirtualText[Ok/Err]
+                    -- "Terminal", --# display results in a vertical split
+                    -- "TerminalWithCode",        --# display results and code history in a vertical split
+                    -- "NvimNotify", --# display with the nvim-notify plugin
+                    -- "Api"                      --# return output to a programming interface
+                },
+            })
+            vim.api.nvim_set_keymap("v", "r", "<Plug>SnipRun", { silent = true })
+            vim.api.nvim_set_keymap("n", "<leader>r", "<Plug>SnipRun", { silent = true })
+        end,
+    },
+
     -- Reference: https://github.com/SirVer/ultisnips
     {
         "honza/vim-snippets",
