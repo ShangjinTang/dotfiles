24f3dbf70568e4e56c828611b3af6e1a2b87939a
Shangjin Tang <shangjin.tang@gmail.com>
2023-07-23 19:56:44 +0800

    Minor: nvim: focus.nvim: disable auto resize on specific buftypes & filetypes

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -894,6 +894,27 @@ lvim.plugins = {
         event = "VeryLazy",
         config = function()
             require("focus").setup({})
+            local ignore_filetypes = { "NvimTree", "Outline", "qf" }
+            local ignore_buftypes = { "nofile", "prompt", "popup", "quickfix" }
+            local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+            vim.api.nvim_create_autocmd("WinEnter", {
+                group = augroup,
+                callback = function(_)
+                    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+                        vim.b.focus_disable = true
+                    end
+                end,
+                desc = "Disable focus autoresize for BufType",
+            })
+            vim.api.nvim_create_autocmd("FileType", {
+                group = augroup,
+                callback = function(_)
+                    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                        vim.b.focus_disable = true
+                    end
+                end,
+                desc = "Disable focus autoresize for FileType",
+            })
         end,
     },
 
