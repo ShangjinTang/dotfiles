36bb597fd03a827dfdf4e56924f59d69a492b692
Shangjin Tang <shangjin.tang@gmail.com>
2023-08-30 16:03:44 +0800

    Major: nvim: autocmd: remove feature 'set no line numbers in insert mode'

--- configs/nvim/lua/autocmd.lua
+++ configs/nvim/lua/autocmd.lua
@@ -43,26 +43,6 @@ vim.api.nvim_create_autocmd({
 --------------------------------------------------------------------------------
 
 vim.api.nvim_create_autocmd({
-    "InsertEnter",
-}, {
-    callback = function()
-        vim.cmd([[ set nonumber ]])
-        vim.cmd([[ set norelativenumber ]])
-    end,
-})
-
-vim.api.nvim_create_autocmd({
-    "InsertLeave",
-}, {
-    callback = function()
-        vim.cmd([[ set number ]])
-        vim.cmd([[ set relativenumber ]])
-    end,
-})
-
---------------------------------------------------------------------------------
-
-vim.api.nvim_create_autocmd({
     "BufNewFile",
     "BufRead",
 }, {
