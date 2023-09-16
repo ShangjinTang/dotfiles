a304110b38138b800a4fbea789896aa1fe52193a
Shangjin Tang <shangjin.tang@gmail.com>
2023-09-16 21:21:53 +0800

    Major: scripts: fix issues reported by linters

--- scripts/python/compile_command_json_filter.py
+++ scripts/python/compile_command_json_filter.py
@@ -1,50 +1,44 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
-import os
-import re
 import json
 import logging
+import os
+import sys
 
 __author__ = "Shangjin Tang"
 __email__ = "shangjin.tang@gmail.com"
-__copyright__ = "Copyright 2022 Shangjin Tang"
+__copyright__ = "Copyright 2023 Shangjin Tang"
 __license__ = "GPL"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
-ARGS = None  # Get argument: ARGS.arg
 LOG_FORMAT = "%(asctime)s    %(levelname)s    %(filename)s:%(lineno)s\n\t%(message)s"
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 
-def main():
+def main(args):
     output_targets = []
-    with open(ARGS.input_file, 'r') as input_json:
+    with open(args.input_file, "r", encoding="utf-8") as input_json:
         input_targets = json.load(input_json)
         for compile_target in input_targets:
-            for pattern in ARGS.filter_pattern:
+            for pattern in args.filter_pattern:
                 if pattern in compile_target["file"]:
                     output_targets.append(compile_target)
-    with open(ARGS.output_file, 'w') as output_json:
+    with open(args.output_file, "w", encoding="utf-8") as output_json:
         json.dump(output_targets, output_json)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter, description="")
-    parser.add_argument('filter_pattern', nargs='+', type=str, action='extend')
-    parser.add_argument('-i',
-                        '--input_file',
-                        default="./compile_commands.json")
-    parser.add_argument('-o',
-                        '--output_file',
-                        default="./compile_commands_output.json")
-    ARGS = parser.parse_args()
+        formatter_class=argparse.RawDescriptionHelpFormatter, description=""
+    )
+    parser.add_argument("filter_pattern", nargs="+", type=str, action="extend")
+    parser.add_argument("-i", "--input_file", default="./compile_commands.json")
+    parser.add_argument("-o", "--output_file", default="./compile_commands_output.json")
     try:
-        main()
+        main(parser.parse_args())
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
--- scripts/python/feature_file_merge.py
+++ scripts/python/feature_file_merge.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import argparse
 import logging
+import os
+import sys
 
 __author__ = "Shangjin Tang"
 __email__ = "shangjin.tang@gmail.com"
@@ -16,7 +16,6 @@ logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 
 class FeatureFileMerger:
-
     def __init__(self, input_files, output_file, priority, file_utils):
         self.features = {}
         self.input_files = input_files
@@ -25,20 +24,19 @@ class FeatureFileMerger:
         self.file_utils = file_utils
 
     def generate(self):
-        if self.priority == 'descending':
+        if self.priority == "descending":
             self.input_files.reverse()
         for file in self.input_files:
             file_features = self.file_utils.load_features_from_file(file)
-            logging.debug("Features read from input file {}: {}".format(
-                file, file_features))
+            logging.debug(f"Features read from input file {file}: {file_features}")
             self.features.update(file_features)
-        logging.debug("Features write to output file {}: {}".format(
-            self.output_file, self.features))
+        logging.debug(
+            "Features write to output file {self.output_file}: {self.features}"
+        )
         self.file_utils.save_features_to_file(self.features, self.output_file)
 
 
 class FileUtils:
-
     def __init__(self, comment_symbol, separate_symbol):
         self.comment_symbol = comment_symbol
         self.separate_symbol = separate_symbol
@@ -46,7 +44,7 @@ class FileUtils:
     def load_features_from_file(self, file):
         features = {}
         if os.path.exists(file):
-            with open(file, 'r') as f:
+            with open(file, "r", encoding="utf-8") as f:
                 line_number = 0
                 for line in f.readlines():
                     line_number += 1
@@ -55,64 +53,61 @@ class FileUtils:
                         line_split = line.split(self.separate_symbol)
                         if len(line_split) != 2:
                             logging.warning(
-                                "file: {} line {}: '{}' is not valid, ignore it"
-                                .format(file, line_number, line))
+                                "file: {} line {}: '{}' is not valid, ignore it".format(
+                                    file, line_number, line
+                                )
+                            )
                             continue
-                        key, value = line_split[0].strip(
-                        ), line_split[1].strip()
+                        key, value = line_split[0].strip(), line_split[1].strip()
                         features[key] = value
         return features
 
     def save_features_to_file(self, features, file):
         logging.debug(os.getcwd())
         logging.debug(file)
-        with open(file, 'w') as f:
+        with open(file, "w", encoding="utf-8") as f:
             for k, v in features.items():
-                line = " ".join([k, self.separate_symbol, v]) + '\n'
+                line = " ".join([k, self.separate_symbol, v]) + "\n"
                 f.writelines(line)
 
 
 def main(args):
     try:
         file_utils = FileUtils(args.comment_symbol, args.separate_symbol)
-        feature_file_merger = FeatureFileMerger(args.input, args.output,
-                                                args.priority, file_utils)
+        feature_file_merger = FeatureFileMerger(
+            args.input, args.output, args.priority, file_utils
+        )
         feature_file_merger.generate()
 
     except KeyboardInterrupt:
-        print('Keyboard Interrupt')
+        print("Keyboard Interrupt")
         try:
             sys.exit(0)
         except SystemExit:
             os._exit(0)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-i',
-                        '--input',
-                        required=True,
-                        nargs="+",
-                        help="Path of input file(s)")
-    parser.add_argument('-o',
-                        '--output',
-                        required=True,
-                        help="Path of output file")
-    parser.add_argument('--priority',
-                        default='ascending',
-                        choices=['ascending', 'descending'],
-                        help="Priority ascending/descending in input files")
-    parser.add_argument('--comment_symbol',
-                        default="#",
-                        help="Comment symbol (default #)")
-    parser.add_argument('--separate_symbol',
-                        default="=",
-                        help="Separate symbol (default =)")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        "-i", "--input", required=True, nargs="+", help="Path of input file(s)"
+    )
+    parser.add_argument("-o", "--output", required=True, help="Path of output file")
+    parser.add_argument(
+        "--priority",
+        default="ascending",
+        choices=["ascending", "descending"],
+        help="Priority ascending/descending in input files",
+    )
+    parser.add_argument(
+        "--comment_symbol", default="#", help="Comment symbol (default #)"
+    )
+    parser.add_argument(
+        "--separate_symbol", default="=", help="Separate symbol (default =)"
+    )
     # parser.add_argument('--ignore-case',
     #                     default=False,
     #                     action='store_true',
     #                     help="Ignore case inside (default: True)")
-    args = parser.parse_args()
-    logging.debug(args)
-    main(args)
+    logging.debug(parser.parse_args())
+    main(parser.parse_args())
