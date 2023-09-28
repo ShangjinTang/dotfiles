ad9cb60740964c06bb3bc751f678c06ffdc2fe47
Shangjin Tang <shangjin.tang@gmail.com>
2023-09-28 22:46:59 +0800

    Minor: nvim: tree-sitter: remove using 'bash' highlight for 'zsh' files

--- configs/nvim/lua/autocmd.lua
+++ configs/nvim/lua/autocmd.lua
@@ -2,12 +2,12 @@
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 
 -- let treesitter use bash highlight for zsh files as well
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "zsh",
-    callback = function()
-        require("nvim-treesitter.highlight").attach(0, "bash")
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "zsh",
+--     callback = function()
+--         require("nvim-treesitter.highlight").attach(0, "bash")
+--     end,
+-- })
 
 --------------------------------------------------------------------------------
 
