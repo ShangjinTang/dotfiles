ff44ca667f714d04a61f0d5ac1fc2900cc380ee7
Shangjin Tang <shangjin.tang@gmail.com>
2023-12-07 18:37:43 +0800

    Major: nvim: flash: add key mappings

--- configs/nvim/config.lua
+++ configs/nvim/config.lua
@@ -700,8 +700,56 @@ lvim.plugins = {
                     flash = true,
                 },
             })
-            require("flash").setup()
+            require("flash").setup({
+                modes = {
+                    char = {
+                        jump_labels = true,
+                    },
+                },
+            })
         end,
+        keys = {
+            {
+                "<c-s>",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Search with Flash",
+            },
+            {
+                "<space>ss",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Search Surroundings with Treesitter",
+            },
+            {
+                "r",
+                mode = "o",
+                function()
+                    require("flash").remote()
+                end,
+                desc = "Remote Flash",
+            },
+            {
+                "R",
+                mode = { "o", "x" },
+                function()
+                    require("flash").treesitter_search()
+                end,
+                desc = "Treesitter Search",
+            },
+            {
+                "<c-s>",
+                mode = { "c" },
+                function()
+                    require("flash").toggle()
+                end,
+                desc = "Toggle Flash Search",
+            },
+        },
     },
 
     -- Reference: https://github.com/rcarriga/nvim-notify
